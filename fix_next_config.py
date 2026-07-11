c46c004cf67a89334075d8314d5f651edfb4c876
ju won bae <juwonbae@MacBook-Pro.local>
2026-07-12 01:48:06 +0900

    chore: clean up python scripts

--- fix_next_config.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('next.config.ts', 'r') as f:
-    config = f.read()
-
-config = re.sub(r"\s*eslint:\s*\{\s*ignoreDuringBuilds:\s*true,?\s*\},", "", config)
-
-with open('next.config.ts', 'w') as f:
-    f.write(config)
